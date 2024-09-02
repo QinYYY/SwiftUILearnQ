a6937e4b3f1d57365b1aefeebfaf4b1bb32edef7
QinYYY <qinyang_c@outlook.com>
2024-09-02 18:19:23 +0930

    map Position

---
+++ SwiftUILearn/BucketList/BucketListView.swift
@@ -0,0 +1,18 @@
+//
+//  BucketListView.swift
+//  SwiftUILearn
+//
+//  Created by QinY on 2/9/2024.
+//
+
+import SwiftUI
+
+struct BucketListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BucketListView()
+}
